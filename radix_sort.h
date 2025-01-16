58be62b5d1554881f96bcce09d4eb2445f4e9247
Mohammad Parsa Elahimanesh <m.p.elahimanesh@gmail.com>
2025-01-16 16:34:14 +0330

    reformat

--- radix_sort.h
+++ radix_sort.h
@@ -1,22 +1,19 @@
 /// In the name of Allah the most beneficent the most merciful
-
 #include <bits/stdc++.h>
 using namespace std;
 
-template<const bool negative = true, const int BITS = 9, typename _RandomAccessIterator>
-inline void radix_sort(_RandomAccessIterator _first, _RandomAccessIterator _last, const bool reverse = false)
-{
-	typedef typename iterator_traits<_RandomAccessIterator>::value_type value_type;
-	if(negative)
-	{
+template<const bool negative = true, const int BITS = 9, typename _Iterator>
+inline void radix_sort(_Iterator _first, _Iterator _last, const bool reverse = false) {
+	typedef typename iterator_traits<_Iterator>::value_type value_type;
+	if(negative) {
 		if(!reverse) {
-			_RandomAccessIterator _middle = partition(_first, _last, [](value_type a) -> bool {return a<0;});	
+			_Iterator _middle = partition(_first, _last, [](value_type a) -> bool {return a<0;});	
 			for(auto it = _first; it != _middle; it++)*it *= -1;
 			radix_sort<false>(_first, _middle, true);
 			for(auto it = _first; it != _middle; it++)*it *= -1;
 			radix_sort<false>(_middle, _last);
 		} else {
-			_RandomAccessIterator _middle = partition(_first, _last, [](value_type a) -> bool {return a>=0;});	
+			_Iterator _middle = partition(_first, _last, [](value_type a) -> bool {return a>=0;});	
 			radix_sort<false>(_first, _middle, true);
 			for(auto it = _middle; it != _last; it++)*it *= -1;
 			radix_sort<false>(_middle, _last);
@@ -29,17 +26,13 @@ inline void radix_sort(_RandomAccessIterator _first, _RandomAccessIterator _last
 	int shift = 0;
 	auto MAX = *max_element(_first, _last);
 	vector<value_type> P[PART];
-	while(log2(MAX) >= shift)
-	{
+	while(log2(MAX) >= shift) {
 		for(auto it = _first; it != _last; it++)P[(((*it)>>shift)&FULL)^(reverse?FULL:0)].push_back(*it);
 		auto it = _first;
-		for(int i = 0; i < PART; i++)
-		{
+		for(int i = 0; i < PART; i++) {
 			for(auto u: P[i]) *it++ = u;
 			P[i].clear();
 		}
 		shift += BITS;
 	}
 }
-
-/// Thank God . . .
